8142261c7ba79bfb56857839b73486427fce4c7d
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-02 18:49:13 -0300

    Add "FAILSAFE" similar to PyAutoGUI
    If the user puts the in the upper left corner of the screen, script will terminate. This is done only in mouse_move because it's the underlying function for mouse inputs (click uses move under the cover).

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,14 @@
 use std::time::Duration;
 use std::{thread, time};
 
+use device_query::{DeviceQuery, DeviceState};
 use rdev::{simulate, Button, EventType, Key, SimulateError};
 
 use crate::coords;
 
 pub fn mouse_move((x, y): (u32, u32)) {
+    check_user_termination();
+
     let x = x + coords::CORNER.0;
     let y = y + coords::CORNER.1;
     send(&EventType::MouseMove {
@@ -36,4 +39,15 @@ fn send(event_type: &EventType) {
     }
     // Let ths OS catchup (at least MacOS)
     thread::sleep(delay);
+}
+
+// In case user wants to stop the running script.
+// As it takes control of the mouse, it may be difficult to stop it
+// through usual means.
+fn check_user_termination() {
+    let device = DeviceState::new();
+    let cursor_position = device.get_mouse().coords;
+    if cursor_position == (0, 0) {
+        panic!("Manual termination through mouse in corner");
+    }
 }
