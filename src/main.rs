473aa3ab4f7b6048d2a39d82def386e290af8958
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-03 13:37:16 -0300

    Add user termination by pressing a key
    We delegate the work of the script to a new thread, and have the main one listening for user input. As soon as the user inputs the special key "Z", the process terminates.
    Eventually we could use this mechanism to change the current action executed by the script, too.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
 use std::{thread, time};
 
+use rdev::{listen, Event, EventType, Key};
+
 mod coords;
 mod input;
 mod inventory;
 
 fn main() {
-    input::mouse_move(*coords::CORNER);
+    // input::mouse_move(*coords::CORNER);
 
-    loop {
-        inventory::merge_equips();
-        inventory::boost_equips();
-        for id in 0..3 {
-            inventory::merge_slot(id as u32);
-            thread::sleep(time::Duration::from_millis(300));
+    thread::spawn(|| {
+        loop {
+            // check_user_termination();
+            inventory::merge_equips();
+            inventory::boost_equips();
+            for id in 0..24 {
+                inventory::merge_slot(id as u32);
+                inventory::boost_slot(id as u32);
+            }
+            thread::sleep(time::Duration::from_secs(10));
         }
-        thread::sleep(time::Duration::from_secs(5));
+    });
+
+    if let Err(e) = listen(check_for_user_termination) {
+        println!("Error listening to events: {:?}", e);
+    }
+}
+
+fn check_for_user_termination(event: Event) {
+    if let EventType::KeyPress(key) = event.event_type {
+        println!("{:?} pressed.", key);
+        if key == Key::KeyZ {
+            println!("Terminating due to user input.");
+            // This hangs the working thread, but OK for now.
+            std::process::exit(0);
+        }
+    } else {
+        println!("Not a key press.");
     }
 }
