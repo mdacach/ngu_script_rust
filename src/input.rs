428d01a3bb879e71431e0d9c9afad4a94b755d1c
Matheus Cardoso <dacachmatheus@gmail.com>
2022-07-06 22:55:11 -0300

    Add alternative way of capturing input presses
    This gets rid of the ugly static Mutex "global variable". Suggestion by: https://github.com/MasterTuto

--- src/input.rs
+++ src/input.rs
@@ -42,7 +42,13 @@ pub fn send_key(key: Key) {
     send(&EventType::KeyRelease(key));
 }
 
-fn release(input: &InputPress) {
+#[derive(PartialEq)]
+pub enum InputPress {
+    Key(Key),
+    Button(Button),
+}
+
+pub fn release(input: &InputPress) {
     match input {
         InputPress::Key(k) => send(&EventType::KeyRelease(*k)),
         InputPress::Button(b) => send(&EventType::ButtonRelease(*b)),
@@ -59,51 +65,4 @@ fn send(event_type: &EventType) {
     }
     // Let the OS catchup (at least MacOS)
     thread::sleep(delay);
-}
-
-lazy_static! {
-    static ref PRESSES: Mutex<Vec<InputPress>> = Mutex::new(Vec::new());
-}
-
-#[derive(PartialEq)]
-enum InputPress {
-    Key(Key),
-    Button(Button),
-}
-
-pub fn handle_user_termination(event: Event) {
-    match event.event_type {
-        EventType::KeyPress(Key::KeyZ) => {
-            // This hangs the working thread, but OK for now.
-            // Note the script will not press Z by itself, so we don't need
-            // to track it in PRESSES for releasing
-            let presses = PRESSES.lock().unwrap();
-            presses.iter().for_each(release);
-            println!("Terminating due to user input.");
-            std::process::exit(0);
-        }
-        EventType::KeyPress(other_key) => {
-            PRESSES.lock().unwrap().push(InputPress::Key(other_key));
-        }
-        EventType::ButtonPress(button) => {
-            PRESSES.lock().unwrap().push(InputPress::Button(button));
-        }
-        EventType::KeyRelease(other_key) => {
-            let mut presses = PRESSES.lock().unwrap();
-            let index = presses
-                .iter()
-                .position(|x| x == &InputPress::Key(other_key))
-                .unwrap();
-            presses.swap_remove(index);
-        }
-        EventType::ButtonRelease(button) => {
-            let mut presses = PRESSES.lock().unwrap();
-            let index = presses
-                .iter()
-                .position(|x| x == &InputPress::Button(button))
-                .unwrap();
-            presses.swap_remove(index);
-        }
-        _ => (),
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{thread, time};
 
 use rdev::{listen, Button, Event, EventType, Key};
 
+use crate::input::{release, InputPress};
 use crate::menu::Menu;
 
 mod adventure;
@@ -25,7 +26,42 @@ fn main() {
         }
     });
 
-    if let Err(e) = listen(input::handle_user_termination) {
+    let mut presses = Vec::new();
+    let event_handler = move |event: Event| {
+        match event.event_type {
+            EventType::KeyPress(Key::KeyZ) => {
+                // This hangs the working thread, but OK for now.
+                // Note the script will not press Z by itself, so we don't need
+                // to track it in PRESSES for releasing
+                presses.iter().for_each(release);
+                println!("Terminating due to user input.");
+                std::process::exit(0);
+            }
+            EventType::KeyPress(other_key) => {
+                presses.push(InputPress::Key(other_key));
+            }
+            EventType::ButtonPress(button) => {
+                presses.push(InputPress::Button(button));
+            }
+            EventType::KeyRelease(other_key) => {
+                let index = presses
+                    .iter()
+                    .position(|x| x == &InputPress::Key(other_key))
+                    .unwrap();
+                presses.swap_remove(index);
+            }
+            EventType::ButtonRelease(button) => {
+                let index = presses
+                    .iter()
+                    .position(|x| x == &InputPress::Button(button))
+                    .unwrap();
+                presses.swap_remove(index);
+            }
+            _ => (),
+        }
+    };
+
+    if let Err(e) = listen(event_handler) {
         println!("Error listening to events: {:?}", e);
     }
 }
