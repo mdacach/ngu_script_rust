92b5413348d6b296e5e7536853711d3afb1aaf21
Matheus Cardoso <dacachmatheus@gmail.com>
2022-08-03 23:40:03 -0300

    Add OCR for Itopod tier rewards counter

--- src/itopod.rs
+++ src/itopod.rs
@@ -3,7 +3,7 @@ use std::thread;
 use crate::adventure;
 use crate::constants::itopod::coords;
 use crate::constants::user::*;
-use crate::input;
+use crate::{input, ocr};
 
 /// Kills `quantity` enemies on ITOPOD's optimal floor.
 /// Requires the game to be in "Adventure" menu.
@@ -82,5 +82,63 @@ pub fn push_itopod() {
         adventure::attack(); // So we attack an extra time
         kill_counter += 1;
         println!("[LOG] Kill counter: {}", kill_counter);
+    }
+}
+
+pub fn enter_itopod_at_floor(floor: u16) {
+    // Enter itopod
+    input::click_at(*coords::ENTER_PIXEL);
+    thread::sleep(LONG_SLEEP);
+
+    // Set starting floor to desired
+    input::click_at(*coords::START_FLOOR_INPUT_PIXEL);
+    thread::sleep(LONG_SLEEP);
+    input::input_number(floor as u64);
+
+    // Confirm Enter
+    input::click_at(*coords::ENTER_CONFIRMATION_PIXEL);
+    thread::sleep(LONG_SLEEP);
+}
+
+pub fn get_current_tier_next_rewards() -> u8 {
+    input::click_at(*crate::constants::itopod::coords::GET_TOOLTIP_PIXEL);
+    thread::sleep(std::time::Duration::from_secs(2));
+    let text =
+        ocr::get_ocr_text_from_area(*crate::constants::itopod::areas::ITOPOD_TOOLTIP_OCR_AREA)
+            .expect("Could not OCR ITOPOD tier rewards");
+
+    let text: Vec<String> = text.split(' ').map(str::to_string).collect();
+    let after_count = text
+        .iter()
+        .position(|x| x.contains("kills."))
+        .expect("Something wrong with ITOPOD OCR");
+    let count = text
+        .get(after_count - 1)
+        .expect("Something wrong with ITOPOD OCR");
+    count
+        .parse::<u8>()
+        .expect("Could not parse ITOPOD kills as integer")
+}
+
+pub fn get_tier_next_rewards_of_floor(floor: u16) -> u8 {
+    enter_itopod_at_floor(floor);
+    get_current_tier_next_rewards()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::menu;
+
+    use super::*;
+
+    #[test]
+    fn test_ocr_itopod_tiers() {
+        menu::navigate(menu::Menu::Adventure);
+
+        println!("{}", get_tier_next_rewards_of_floor(0));
+        println!("{}", get_tier_next_rewards_of_floor(50));
+        println!("{}", get_tier_next_rewards_of_floor(100));
+        println!("{}", get_tier_next_rewards_of_floor(150));
+        println!("{}", get_tier_next_rewards_of_floor(200));
     }
 }
